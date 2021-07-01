78da68de48ff356a30cd3b50ade3d2ec2f59ff60
Vipansh Thakur <vipansh@github.com>
2021-07-01 21:02:46 +0530

    port update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MONGODB } = require("./config.js");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.port || 1234;
 
 const pubsub = new PubSub();
 
